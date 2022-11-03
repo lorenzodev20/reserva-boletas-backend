9da1ab1f8fa98f6ff8314862aef5ed51eebfa5bb
Lorenzo Rojo <lorenzorojo12@gmail.com>
2022-11-03 14:14:00 -0500

    Start Update

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use App\Http\Resources\CustomerResource;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Requests\CustomerRequest;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
@@ -24,8 +26,8 @@ class CustomerController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param CustomerRequest $request
+     * @return JsonResponse
      */
     public function store(CustomerRequest $request)
     {
@@ -39,8 +41,8 @@ class CustomerController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
+     * @param Customer $customer
+     * @return Customer
      */
     public function show(Customer $customer)
     {
@@ -50,20 +52,28 @@ class CustomerController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
+     * @param Customer $customer
+     * @param $id
+     * @return JsonResponse
      */
-    public function update(Request $request, Customer $customer)
+    public function update(Customer $customer, CustomerRequest $request, $id): JsonResponse
     {
-        //
+        $customerFind = Customer::find($id);
+
+        if (!$customerFind){
+            return response()->json(['message' => 'Customer not found', 'errors' => $customerFind], 404);
+        }
+        /*if ($rps) {
+            return response()->json(['message' => 'Customer create succesfully', 'result' => $rps], 201);
+        }*/
+        return response()->json(['message' => 'Error to update customer', 'errors' => $customerFind], 500);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
+     * @param Customer $customer
+     * @return JsonResponse
      */
     public function destroy(Customer $customer)
     {
